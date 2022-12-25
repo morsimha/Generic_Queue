d1e596e14d460577202e9553dd12acda47b6368c
morsimha <mor.simha@intel.com>
2022-12-25 20:20:38 +0200

    first commit

---
+++ src/MyQueue.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MyQueue {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static <E extends Reducable <E>>ArrayList<E> reduceAll(ArrayList<E> a){
+        return a;
+    }
+}
