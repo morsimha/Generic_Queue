82116f735aa50baf316327dc5dc96e5bac4fa0b7
morsimha <mor.simha@intel.com>
2022-12-25 23:12:53 +0200

    all beside iterator

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,61 +1,164 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class MyQueue <T>{
-    private ArrayList<T> q2;
-    private Queue<T> q;
+public class MyQueue <T> implements Iterable<T> {
+    private ArrayList<LinkedList<T>> q;
     private int maxPriority;
 
-    public MyQueue(int n){
-        q2 = new ArrayList<T>();
-        q = new LinkedList<T>();
+    public MyQueue(int n) {
         maxPriority = n;
-    }
-    public void add(T t, int newPriority){
-        if (newPriority <= maxPriority) // valid priority
-            if (q2.get(newPriority) != null){
-                //need to push one right
+        q = new ArrayList<LinkedList<T>>(11);
+        for (int i = 1; i <= 11; i++) {
+            q.add(new LinkedList<>());
         }
-        else // insert as last element
-            q2.set(maxPriority,t);
     }
 
-    public void poll(T t, int newPriority){
-        if (newPriority <= maxPriority) // valid priority
-            q.add(t); //TODO need to add to the correct priority
-        else // insert as last element
-            q.add(t);
+    public void add(T t, int currPriority) {
+        if (maxPriority < currPriority) // invalid priority
+            currPriority = maxPriority;
+        q.get(currPriority).addLast(t); //keeping the first entered always first
+    }
+
+    public T poll() {
+        T mostPrior = null;
+        for (int i = 1; i <= 10; i++) {
+            mostPrior = q.get(i).poll();
+            if (mostPrior != null)
+                return mostPrior;
+        }
+        return mostPrior;
+    }
+
+    public boolean contains(T t) {
+        for (int i = 1; i <= 10; i++)
+            if (q.get(i).contains(t))
+                return true;
+        return false;
+    }
+
+    public boolean remove(T element) {
+        for (int i = 1; i <= 10; i++)
+            if (q.get(i).remove(element))
+                return true;
+        return false;
+    }
+
+    public int size() {
+        int size = 0;
+        for (int i = 1; i <= 10; i++) {
+            size += q.get(i).size();
+        }
+        return size;
+    }
+
+
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> it = new Iterator<T>() {
+            private int currentIndex = 1;
+
+            @Override
+            public boolean hasNext() {
+                for (int i = currentIndex; i <= maxPriority; i++) {
+                    try {
+                        return q.get(currentIndex).iterator().hasNext();
+                    }
+                    catch (Exception e){
+                        continue;
+                    }
+//                    if (q.get(currentIndex).iterator().hasNext())
+//                        return true;
+                }
+                return false;
+            }
+
+            @Override
+            public T next() {
+                for (int i = currentIndex; i <= maxPriority; i++) {
+                    try {
+                        return q.get(currentIndex).iterator().next();
+                    }
+                    catch (Exception e){
+                        continue;
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
     }
 
 
 
 
 
+//
+//    @Override
+//    public Iterator<T> iterator() {
+//        return new MyQueueIterator();
+//    }
+//
+//    public class MyQueueIterator <T>  implements Iterator<T> {
+//        private int listIndex;
+//        private Iterator<T> listIterator;
+//
+//        public MyQueueIterator() {
+//            listIndex = 0;
+//            listIterator = q.get(listIndex).iterator();
+//        }
+//        @Override
+//        public boolean hasNext() {
+//            return q.get(listIndex).iterator();
+//        }
+//
+//        @Override
+//        public T next() {
+//            return null;
+//        }
+//    }
 
-
-
-
-
-    public static void main(String[] args) {
+public static void main (String[]args){
         System.out.println("Hello world!");
+    MyQueue<String> q = new MyQueue<String>(10);
+
+
+    q.add("High priority element",5);
+    q.add( "High priority element2",1);
+    System.out.println(q.size());
+    Iterator<String> it = q.iterator();
+    while(it.hasNext()){
+        System.out.println(it.next());
     }
 
-    public Queue<T> getQ() {
-        return q;
-    }
 
-    public void setQ(Queue<T> q) {
-        this.q = q;
-    }
 
-    public int getPriority() {
-        return maxPriority;
-    }
 
-    public void setPriority(int priority) {
-        maxPriority = priority;
+
     }
+//
+//        public Queue<T> getQ () {
+//            return q;
+//        }
+//
+//        public void setQ (Queue < T > q) {
+//            this.q = q;
+//        }
+//
+//        public int getPriority () {
+//            return maxPriority;
+//        }
+//
+//        public void setPriority ( int priority){
+//            maxPriority = priority;
+//        }
 
 //    public static <E extends Reducable <E>>ArrayList<E> reduceAll(ArrayList<E> a){
 //        return a;
--- src/MyQueueIterator.java
+++ src/MyQueueIterator.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class MyQueueIterator {
-}
+//import java.util.Iterator;
+//import My
+//
+//public class MyQueueIterator <T>  implements Iterator<T> {
+//
+//    public QueueIterator() {
+//            listIndex = 0;
+//            listIterator = q.get(listIndex).iterator();
+//        }
+//    @Override
+//    public boolean hasNext() {
+//        return q.get(listIndex).iterator();
+//    }
+//
+//    @Override
+//    public T next() {
+//        return null;
+//    }
+//}
+
+//    private class QueueIterator implements Iterator<T> {
+//        private int listIndex;
+//        private Iterator<T> listIterator;
+//
+//        public QueueIterator() {
+//            listIndex = 0;
+//            listIterator = q.get(listIndex).iterator();
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            if (listIterator.hasNext()) {
+//                return true;
+//            }
+//            for (int i = listIndex + 1; i < q.size(); i++) {
+//                if (!q.get(i).isEmpty()) {
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
+//
+//        @Override
+//        public T next() {
+//            if (listIterator.hasNext()) {
+//                return listIterator.next();
+//            }
+//            for (int i = listIndex + 1; i < q.size(); i++) {
+//                if (!q.get(i).isEmpty()) {
+//                    listIndex = i;
+//
+//                }
